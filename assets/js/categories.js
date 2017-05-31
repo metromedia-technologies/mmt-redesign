7357e7593e007d74cfc480e42fa51bfe107c0684
Joseph Sams <sams_josep@yahoo.com>
2017-05-31 15:45:16 -0400

    Added favicon

--- assets/js/categories.js
+++ assets/js/categories.js
@@ -5,13 +5,12 @@
 
   const $productLink = $('.product__link');
   const $productDetails = $('.product__details');
+  const $productBKG = $('.product__bkg');
 
   $productLink.hover(function() {
     $(this).find('.product__details').addClass('showing');
-    $(this).find('img').addClass('zoom-blur');
   }, function() {
     $(this).find('.product__details').removeClass('showing');
-    $(this).find('img').removeClass('zoom-blur');
   });
 
 
