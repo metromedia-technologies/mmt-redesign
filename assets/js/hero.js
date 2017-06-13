7fbd558f8cdaa416a344785aa4e734da48ef663c
Joseph Sams <sams_josep@yahoo.com>
2017-06-13 14:07:36 -0400

    Added transition to hero image

--- assets/js/hero.js
+++ assets/js/hero.js
@@ -4,8 +4,10 @@
 (($) => {
 
 const headlineLockup = $('.headline__lockup');
+const heroCoverPhoto = $('.hero__cover-photo');
 
 $(window).on('load', () => {
+  heroCoverPhoto.removeClass('blur-hero-image');
   headlineLockup.removeClass('hide-headline');
 });
 
